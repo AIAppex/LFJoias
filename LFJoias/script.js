58250c81c23f4ec9f1a6db446ff79b6557cb53f7
AIAppex <166263748+AIAppex@users.noreply.github.com>
2024-07-18 14:36:37 -0300

    Add files via upload

---
+++ LFJoias/script.js
@@ -0,0 +1,17 @@
+// // JavaScript
+// document.querySelectorAll('.box-im img').forEach(img => {
+//     img.addEventListener('click', function() {
+//         const expandedImg = document.createElement('img');
+//         expandedImg.src = this.src;
+//         expandedImg.classList.add('expanded-img');
+//         document.body.appendChild(expandedImg);
+//         document.querySelector('.colecao').classList.add('blur-background');
+
+//         expandedImg.addEventListener('click', function() {
+//             expandedImg.remove();
+//             document.querySelector('.colecao').classList.remove('blur-background');
+//         });
+//     });
+// });
+
+// OBS: Iremos desenvolver melhor essa opcão em breve
